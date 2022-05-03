59e3d97639cc9f58052e7c2d74cbcc8b6da8ee88
Ahmed Orabi <aorabi@outlook.com>
2022-05-03 16:07:06 +0300

    type of

--- build/test.js
+++ build/test.js
@@ -1,9 +1,11 @@
 "use strict";
 const a = 4;
 const b = 6;
-const c = '5';
+const c = parseInt('5');
 const d = 'cat';
 const add = (num1, num2) => {
+    console.log(`type of Num1`, typeof num1);
+    console.log(`type of Num2`, typeof num2);
     console.log(`${num1} + ${num2} = ${num1 + num2} `, num1 + num2);
 };
 add(a, c);
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,13 @@
 const a = 4;
 const b = 6;
-const c = ('5' as unknown) as any;
+const c = parseInt('5') as number;
 const d = 'cat';
 
 const add = (num1: number, num2: number) => {
+
+    console.log(`type of Num1`, typeof num1)
+    console.log(`type of Num2`, typeof num2)
+
 
     console.log(`${num1} + ${num2} = ${num1 + num2} `, num1 + num2)
 }
